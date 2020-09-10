8fe016a7321ae07c8d86b88d807ab4c4b60c10c3
Juan Carlos Aleman Mayorga <jmayorga92@hotmail.com>
2020-09-10 01:43:38 -0600

    Se agrega reposotrio generico base para los controlladores Se elimina intefaz que se uso de muestra, Se prueba el CRUD funcionalidad correcta

--- apivent/Controllers/PersonaController.cs
+++ apivent/Controllers/PersonaController.cs
@@ -2,62 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 using apivent.Application.Interfaces;
 using System.Collections.Generic;
 using apivent.Infrastructure.Models;
+using apivent.Application.Repositories;
 
 namespace apivent.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PersonaController : ControllerBase, IControllerBase<Persona>
+    public class PersonaController : ControllerBaseRepository<Persona> 
     {
         private readonly IPersonaService repoPersona;  
 
-        public PersonaController(IPersonaService _repoPersona)
+        public PersonaController(IPersonaService _repoPersona) :base(_repoPersona)
         {
             repoPersona = _repoPersona;
         }
-
-        [HttpPost]
-        public IActionResult PostCreate([FromBody] Persona record)
-        {
-            repoPersona.Add(record);
-            repoPersona.Save();
-            return Ok(record);
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            repoPersona.Delete(id);
-            if (repoPersona.Save() == 0)
-                return BadRequest();
-
-            return Ok(id);    
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetFind(int id)
-        {
-            var record = repoPersona.GetById(id);
-            if (record == null)
-                return NotFound();
-
-            return Ok(record);
-        }
-
-        [HttpGet]
-        [Route("")]
-        public IActionResult Get() => Ok(repoPersona.Get());
-
-        [HttpPut("{id}")]
-        public IActionResult PutUpdate(int id, [FromBody] Persona record)
-        {
-            repoPersona.Update(record);
-            if (repoPersona.Save() == 0)
-                return BadRequest();
-            
-            return Ok(record);
-        }
-
 
         [HttpGet]
         [Route("tipo/{tipo}")]
